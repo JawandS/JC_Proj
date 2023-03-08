ab9a8b35e64b4015cba36b482e8cf5f9681b6568
JawandS <JawandSingh@gmail.com>
2023-03-08 19:43:36 +0000

    update proj

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def my_app():
-    return 'First Flask application!'
+def home():
+   return render_template('home.html')
+if __name__ == '__main__':
+   app.run(host="127.0.0.1", port=5000)
